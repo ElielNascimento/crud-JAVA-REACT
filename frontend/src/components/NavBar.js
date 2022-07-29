917eaf3a1a6941d30ef29d65062dcf90451e1de5
ElielNascimento <nascimentob.eliel@gmail.com>
2022-07-28 22:48:41 -0300

    atualizado git no front

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
             <nav className="container">
                 <div className="crud-nav-content">
                     <h1> Dev Eliel B. Nascimento</h1>
-                    <a href="https://github.com/ElielNascimento/crudFullStackJavaReact" alt="test">
+                    <a href="https://github.com/ElielNascimento/crud-JAVA-REACT" alt="test">
                         <div className="dsmovie-contact-container">
                             <p> /elielnascimento</p>
                         </div>
